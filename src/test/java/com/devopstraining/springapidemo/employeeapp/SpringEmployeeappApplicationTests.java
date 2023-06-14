ce7d5e5a2f064ae01eb317d7967368a8ba055ed0
mrachuta <34251394+mrachuta@users.noreply.github.com>
2023-06-14 23:35:51 +0200

    Update pipeline; fix issues with tests

--- src/test/java/com/devopstraining/springapidemo/employeeapp/SpringEmployeeappApplicationTests.java
+++ src/test/java/com/devopstraining/springapidemo/employeeapp/SpringEmployeeappApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringapidemoApplicationTests {
+class SpringEmployeeappApplicationTests {
 
 	@Test
 	void contextLoads() {
