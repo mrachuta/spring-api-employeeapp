9833c0aeea894f23788f89ff036b294e2c1ac52a
mrachuta <34251394+mrachuta@users.noreply.github.com>
2025-02-05 19:52:00 +0100

    Update packages and rename artifact

--- src/main/java/com/devopstraining/springapidemo/employeeapp/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devopstraining.springapidemo.employeeapp.repository;
-
-import com.devopstraining.springapidemo.employeeapp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
